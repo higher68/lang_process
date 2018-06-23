471c5b4151ae7b57e81af0a79d710d84f2156334
higher68 <hatiro7code@gmail.com>
2018-06-23 11:33:10 +0900

    45 coding

--- 44.py
+++ 44.py
@@ -89,3 +89,140 @@ def neco_lines():
     戻り値：
     １文のChunkクラスのリスト
     '''
+    with open(fname_parsed) as file_parsed:
+
+        chunks = dict()  # idxをkeyにChunkを格納
+        idx = -1
+
+        for line in file_parsed:
+
+            # 1文の終了判定
+            if line == 'EOS\n':
+
+                # Chunkのリストを返す
+                if len(chunks) > 0:
+
+                    # chunksをkeyでソートし、valueのみ取り出し
+                    sorted_tuple = sorted(chunks.items(), key=lambda x: x[0])
+                    # items()タプルを生成
+                    # タプルは変更不可、リストは書き換え可
+                    yield list(sorted_tuple)
+                    chunk.clear()
+
+                else:
+                    yield []
+            # 先頭が*の行は係り受け解析結果なので、Chunkを生成.
+            elif line[0] == '*':
+
+                # Chunkのインデックス番号と係り受け先のインデックス番号取得
+                cols = line.split(' ')
+                idx = int(cols[1])
+                dst = int(re.search(r'(.*?)D', cols[2]).group(1))
+                # http://d.hatena.ne.jp/yumimue/20071220/1198141598
+                # 一番目のマッチしたグループ取り出し。
+                # Morphオブジェクト)のリスト(morphs)、
+                # 係り受け文節インデックス番号(dst)、
+                # 係り元文節インデックス番号のリスト(srcs)
+
+                # Chunkを生成(なければ)し、係り先のインデックス番号セット
+                if idx not in chunks:
+                    # 指定の値が存在してたらTrue
+                    chunks[idx] = Chunk()
+                chunks[idx].dst = dst
+                # 係り先のChunkを生成(なければ)し、係り元インデックス番号追加
+                if dst != -1:
+                    if dst not in chunks:
+                        chunks[dst] = Chunk()
+                    chunks[dst].srcs.append(idx)
+                # 要するに、解析結果があったら、前後をみてその情報を入れてるだけか
+
+            # それ以外の行は形態素解析結果なので、Morphを作りChunkに追加
+        else:
+
+            # 表層形はtab区切り、それ以外は','区切り。
+            cols = line.split('\t')
+            res_cols = cols[1].split(',')
+
+            # Morph作成、リストに追加
+            chunks[idx].morph.append(
+                Morph(
+                    cols[0],    # suraface
+                    res_cols[6],  # base
+                    res_cols[0],  # pos
+                    res_cols[1]  # pos1
+                )
+            )
+
+
+def graph_from_edge_ex(edge_list, directed=False):
+    '''pydot_ng.graph_from_edges()のノード識別子への対応版
+
+    graph_from_edges()のedge_listで指定するタプルは
+    識別子とグラフ表示時のラベルが同一のため、
+    ラベルが同じだが実体が異なるノードを表現することができない。
+    例えば文の係り受けをグラフにする際、文の中に同じ単語が
+    複数出てくると、それらのノードが同一視されて接続されてしまう。
+
+    この関数ではedge_listとして次の書式のタプルを受け取り、
+    ラベルが同一でも識別子が異なるノードは別ものとして扱う。
+
+    edge_list = [((識別子1,ラベル1),(識別子2,ラベル2)), ...]
+
+    識別子はノードを識別するためのもので表示されない。
+    ラベルは表示用で、同じでも識別子が異なれば別のノードになる。
+
+    なお、オリジナルの関数にあるnode_prefixは未実装。
+
+    戻り値：
+    pydot.Dotオブジェクト
+    '''
+
+    if directed:
+        graph = pydot.Dot(graph_type='digraph')
+
+    else:
+        graph = pydot.Dot(graph_type='graph')
+
+    for edge in edge_list:
+
+        id1 = str(edge[0][0])
+        label1 = str(edge[0][1])
+        id2 = str(edge[1][0])
+        label2 = str(edge[1][1])
+
+        # ノード追加
+        graph.add_node(pydot.Node(id1, label=label1))
+        graph.add_node(pydot.Node(id2, label=label2))
+
+        # エッジ追加
+        graph.add_edge(pydot.Edge(id1, id2))
+
+    return graph
+
+
+# 対象文字列を入力してもらい、そのままfnameに保存
+with open(fname, mode='w') as out_file:
+    out_file.write(input('文字列を入力してください--> '))
+
+# 係り受け解析
+parse_neko()
+
+# 1文ずつリスト作成
+for chunks in neco_lines():
+
+    # 係り先があるものを列挙
+    edges = []
+    for i, chunk in enumerate(chunks):
+        if chunk.dst != -1:
+
+            # 記号を除いた表層形をチェック、空なら除外
+            src = chunk.normalized_surface()
+            dst = chunks[chunk.dst].normalized_surface()
+            if src != '' and dst != '':
+                edges.append(((i, src), (chunk.dst, dst)))
+
+    # 描画
+    if len(edges) > 0:
+        graph = graph_from_edge_ex(edges, directed=True)
+        # directed=Trueだと、有向グラフとなり、エッジが矢印になる
+        graph.write_png('result.png')
