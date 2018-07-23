d436bb5383e39d2ffb8481545758a980698b6e05
higher68 <hatiro7code@gmail.com>
2018-07-24 07:55:58 +0900

    20180724

--- 54.py
+++ 54.py
@@ -1,39 +1,41 @@
+
 import os
 import subprocess
-import xml.etree.ElementTree as ET  # XML データを解析および作成するシンプルかつ効率的な API
+import xml.etree.ElementTree as ET
 
 fname = 'nlp.txt'
 fname_parsed = 'nlp.txt.xml'
 
 
 def parse_nlp():
-    '''nlp.txtをStanford Core NLPで解析、xmlファイルへ出力結果ファイルが
-    すでに存在する時には実行しない
+    '''nlp.txtをStanford Core NLPで解析しxmlファイルへ出力
+    すでに結果ファイルが存在する場合は実行しない
     '''
-
     if not os.path.exists(fname_parsed):
 
-        # StanfordCoreNLP実行、標準エラーparse.outに出力
+        # StanfordCoreNLP実行、標準エラーはparse.outへ出力
         subprocess.run(
-            'java -cp "/Users/junya/Library/stanford-corenlp-full-2018-02-27/*"'
+            'java -cp "/usr/local/lib/stanford-corenlp-full-2016-10-31/*"'
             ' -Xmx2g'
             ' edu.stanford.nlp.pipeline.StanfordCoreNLP'
             ' -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref'
             ' -file ' + fname + ' 2>parse.out',
-            shell=True,  # ,   # shell
+            shell=True,     # shellで実行
             check=True      # エラーチェックあり
         )
 
 
 # nlp.txtを解析
 parse_nlp()
-print('hoge')
-print('hoge2')
+
 # 解析結果のxmlをパース
 root = ET.parse(fname_parsed)
-print('hoge3')
 
-# wordのみ取り出し
-for word in root.iter('word'):
-    exit()
-    print(word.text)
+# tokenの抽出
+for token in root.iter('token'):
+
+    # 単語、レンマ、品詞の抽出
+    word = token.findtext('word')
+    lemma = token.findtext('lemma')
+    pos = token.findtext('POS')
+    print('{}\t{}\t{}'.format(word, lemma, pos))
