b92822308102ff0f8ee293fbc951c42c9058fd64
higher68 <hatiro7code@gmail.com>
2018-07-24 07:55:52 +0900

    20180724

--- 53.py
+++ 53.py
@@ -1,40 +1,38 @@
 import os
 import subprocess
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET  # XML データを解析および作成するシンプルかつ効率的な API
 
 fname = 'nlp.txt'
 fname_parsed = 'nlp.txt.xml'
 
 
 def parse_nlp():
-    '''nlp.txtをStanford Core NLPで解析しxmlファイルへ出力
-    すでに結果ファイルが存在する場合は実行しない
+    '''nlp.txtをStanford Core NLPで解析、xmlファイルへ出力結果ファイルが
+    すでに存在する時には実行しない
     '''
-    if not os.path.exists(fname_parsed):
 
-        # StanfordCoreNLP実行、標準エラーはparse.outへ出力
+    if not os.path.exists(fname_parsed):
+        'java -cp "/usr/local/lib/stanford-corenlp-full-2016-10-31/*"'
+        ' -Xmx2g'
+        ' edu.stanford.nlp.pipeline.StanfordCoreNLP'
+        ' -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref'
+        ' -file ' + fname + ' 2>parse.out',
+        # StanfordCoreNLP実行、標準エラーparse.outに出力
         subprocess.run(
-            'java -cp "/usr/local/lib/stanford-corenlp-full-2016-10-31/*"'
-            ' -Xmx2g'
-            ' edu.stanford.nlp.pipeline.StanfordCoreNLP'
-            ' -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref'
-            ' -file ' + fname + ' 2>parse.out',
-            shell=True,     # shellで実行
-            check=True      # エラーチェックあり
+    # Xmx3gにしないとヒープ領域が足りなくなるよ
+            shell=True,  # ,   # shell
+            check=True      # エラーチェックあり   # エラーチェックあり
         )
 
 
 # nlp.txtを解析
 parse_nlp()
-
 # 解析結果のxmlをパース
 root = ET.parse(fname_parsed)
-
-# tokenの抽出
-for token in root.iter('token'):
-
-    # 単語、レンマ、品詞の抽出
-    word = token.findtext('word')
-    lemma = token.findtext('lemma')
-    pos = token.findtext('POS')
-    print('{}\t{}\t{}'.format(word, lemma, pos))
+# print(type(root))
+# wordのみ取り出し
+# iter()イテレータオブジェクトを返す
+for word in root.iter('word'):
+    # 公式によると、'word'が現れるまで読み進めているらしい。上は
+    exit()
+    print(word.text)
