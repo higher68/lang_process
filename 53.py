2e9dc7c51d38b2dda4830b97cd3568d62870f424
higher68 <hatiro7code@gmail.com>
2018-07-24 12:45:59 +0900

    20180724_understood

--- 53.py
+++ 53.py
@@ -1,3 +1,4 @@
+# xmlはなんか文書の構造が書いてあるもの
 import os
 import subprocess
 import xml.etree.ElementTree as ET  # XML データを解析および作成するシンプルかつ効率的な API
@@ -19,7 +20,7 @@ def parse_nlp():
         ' -file ' + fname + ' 2>parse.out',
         # StanfordCoreNLP実行、標準エラーparse.outに出力
         subprocess.run(
-    # Xmx3gにしないとヒープ領域が足りなくなるよ
+            # Xmx3gにしないとヒープ領域が足りなくなるよ
             shell=True,  # ,   # shell
             check=True      # エラーチェックあり   # エラーチェックあり
         )
@@ -28,11 +29,10 @@ def parse_nlp():
 # nlp.txtを解析
 parse_nlp()
 # 解析結果のxmlをパース
-root = ET.parse(fname_parsed)
+root = ET.parse(fname_parsed)  # 要素の木にする
 # print(type(root))
 # wordのみ取り出し
 # iter()イテレータオブジェクトを返す
-for word in root.iter('word'):
+for word in root.iter('word'):  # もしかして、wordのタグがついた要素のみでイテレーターを作ってるのか？
     # 公式によると、'word'が現れるまで読み進めているらしい。上は
-    exit()
-    print(word.text)
+    print(word.text)  # 原文まま。テキストを拾ってきてるらしい
