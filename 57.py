dfb166d571d32f4c4e18e35b35dcb57c545c17db
higher68 <hatiro7code@gmail.com>
2018-07-29 11:53:58 +0900

    20180729_understood

--- 57.py
+++ 57.py
@@ -25,7 +25,7 @@ def parse_nlp():
         )
 
 
-def graph_from_edges_ex(edge_list , directed=False):
+def graph_from_edges_ex(edge_list, directed=False):
     '''pydot_ng.graph_from_edges()のノード識別子への対応版
 
     graph_from_edges()のedge_listで指定するタプルは
@@ -47,10 +47,64 @@ def graph_from_edges_ex(edge_list , directed=False):
     戻り値：
     pydot.Dotオブジェクト
     '''
+    # pydot使い方https://qiita.com/segavvy/items/d1a9a8d87d8dc10a8f15
+    # タプルに入れたノーどのペアがエッジで結ばれる
+    if directed:  # なんかここよくわかんねえな
+        graph = pydot.Dot(graph_type='digraph')
+
+    else:
+        graph = pydot.Dot(graph_type='graph')
+
+    for edge in edge_list:
+
+        id1 = str(edge[0][0])
+        label1 = str(edge[0][1])
+        id2 = str(edge[1][0])
+        label2 = str(edge[1][1])
+
+        # ノーど追加
+        graph.add_node(pydot.Node(id1, label=label1))
+        graph.add_node(pydot.Node(id2, label=label2))
+
+        # エッジ追加
+        graph.add_edge(pydot.Edge(id1, id2))
+
+    return graph
 
 
-# nlp.txtを解析
+# nlp.txt()
 parse_nlp()
 
 # 解析結果のxmlをパース
 root = ET.parse(fname_parsed)
+# governorが係り受け元、dependentが係り受け先
+# sentenceの層の中のdependenciesの中に
+# dependentとgovornorの情報が入っている
+# ff = 0
+for sentence in root.iterfind('./document/sentences/sentence'):
+    # ff += 1
+    # if ff == 2:
+    #     exit()
+    sent_id = int(sentence.get('id'))   # sentenceのid
+
+    edges = []
+    # dependencies列挙
+    for dep in sentence.iterfind(
+        './dependencies[@type="collapsed-dependencies"]/dep'
+    ):
+        # depでプリントしようとしても、層だから言葉は入ってない
+        # キーワードを絞って検索範囲をどんどん絞ってるのかあ
+        # 句読点はスキップ
+        if dep.get('type') != 'punct':
+
+            # governor、dependent取得、edgesに追加
+            govr = dep.find('./governor')
+            dept = dep.find('./dependent')
+            edges.append(
+                ((govr.get('idx'), govr.text), (dept.get('idx'), dept.text))
+            )
+            # print((govr.get('idx'), govr.text), (dept.get('idx'), dept.text))
+
+    if len(edges) > 0:
+        graph = graph_from_edges_ex(edges, directed=True)
+        graph.write_png('{}.png'.format(sent_id))
