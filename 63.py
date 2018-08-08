d9c9fa34504a5ff8136c5efb274d220d58fcf627
higher68 <hatiro7code@gmail.com>
2018-08-08 23:15:03 +0900

    20180808_understood

--- 63.py
+++ 63.py
@@ -30,14 +30,43 @@ try:
 
             # name+idとtagsをDBへ追加
             key = data_json['name'] + '\t' + str(data_json['id'])
-            valud = data_json.get('tags')       # tagsはないことがある
-            if value in None:
+            value = data_json.get('tags')       # tagsはないことがある
+            if value is None:
                 value = []
             db.Put(key.encode(), json.dumps(value).encode())
+            # json.dumpsでjson形式で格納できる
     # 確認のため登録件数表示
     print('{}件登録'.format(len(list(db.RangeIter(include_value=False)))))
+
 except:
     db = leveldb.LevelDB(fname_db)
     print('既存のDBを使います。')
 
 # 条件入力
+clue = input('アーティスト名を入力してください--> ')
+hit = False
+
+# アーティスト名 + '\t'で検索
+for key, value in db.RangeIter(key_from=(clue + '\t').encode()):
+    # keyをnameとidに戻す
+    match = pattern.match(key.decode())
+    name = match.group(1)
+    id = match.group(2)
+
+    # 異なるアーティストになったら終了
+    if name != clue:
+        break
+    print(value, value.decode())
+    # タグ情報取得
+    tags = json.loads(value.decode())
+    # json.loads()で、json形式の文字列を辞書型に変換
+    print('{}(id:{})のタグ情報:'.format(name, id))
+    if len(tags) > 0:
+        for tag in tags:
+            print('\t{}({})'.format(tag['value'], tag['count']))
+    else:
+        print('\tタグはありません')
+    hit = True
+
+if not hit:
+    print('{}は登録されていません'.format(clue))
