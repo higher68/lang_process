45ed88b351399da34c88263f9d67df27054a0b48
higher68 <hatiro7code@gmail.com>
2018-08-08 22:51:52 +0900

    20180808_understood

--- 62.py
+++ 62.py
@@ -6,5 +6,11 @@ db = leveldb.LevelDB(fname_db)
 # valueが日本のものを列挙
 clue = 'Japan'.encode()
 result = [value[0].decode() for value in db.RangeIter() if value[1] == clue]
+# リスト内包表記について
+# リスト内に限って、ifを後置することができる。
+# for value in db.RangeIter():
+#   if value[1] == clue:
+#       value[0].decode()
+# と同じ
 # print(result)
 print('{}件'.format(len(result)))
