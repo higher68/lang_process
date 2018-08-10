5885eb354e041f2e82d1f22c1347dbf5bd9be4b1
higher68 <hatiro7code@gmail.com>
2018-08-10 14:20:47 +0900

    20180810_processing_pymongoerror

--- 64.py
+++ 64.py
@@ -5,11 +5,11 @@ from pymongo import MongoClient
 
 fname = 'artist.json.gz'
 unit_bulk = 10000  # バルクインサートする単位(件)
-
+# バルクインサートをすると、insertしたドキュメントがリスト化される？
 # MongoDBのデータベースtestdbにコレクションartistを作成
 client = MongoClient()
-db = client.testdb
-collection = db.artist
+db = client.testdb  # testdbというデータベースを作る
+collection = db.artist  # artistというコレクションを作る
 
 # gzファイル読み込み
 with gzip.open(fname, 'rt') as data_file:
@@ -17,7 +17,9 @@ with gzip.open(fname, 'rt') as data_file:
     # 1行ずつパースしてbufに詰め込む
     buf = []
     for i, line in enumerate(data_file, 1):
+        # enumerate(対象, 開始するインデックス番号)
         data_json = json.loads(line)
+        # lineの中身をjson形式から普通のにする?
         buf.append(data_json)
 
         # unit_bulk件溜まったらartistへバルクインサート
@@ -32,3 +34,9 @@ with gzip.open(fname, 'rt') as data_file:
 
 
 # インデックス作成
+collection.create_index([('name', pymongo.ASCENDING)])
+# collection.create_indexで、インデックスをデータに対して作れる?
+collection.create_index([('aliases.name', pymongo.ASCENDING)])
+collection.create_index([('tags.value', pymongo.ASCENDING)])
+collection.create_index([('rating.value', pymongo.ASCENDING)])
+# ascendig；昇順、descending：降順
