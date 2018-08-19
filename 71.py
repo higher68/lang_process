f08cf2a7db6b2dec71ff7f295ca611781977ebc2
junya <hatiro7code@gmail.com>
2018-08-20 07:55:18 +0900

    20180820 before test

---
+++ 71.py
@@ -0,0 +1,42 @@
+# stop-word：文章から特徴を抽出する際に対象外となる単語
+# あまり意味をなさない単語
+# 日本語の場合は、「は」、「を」などが相当する。
+# ストップワードのリスト  http://xpo6.com/list-of-english-stop-words/ のCSV Formatより
+# tuple:(, , )と書く。イミュータブル
+# list:[]で書く。ミュータブル
+stop_words = (
+    'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,'
+    'as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,'
+    'either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,'
+    'him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,'
+    'likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,'
+    'on,only,or,other,our,own,rather,said,say,says,she,should,since,so,'
+    'some,than,that,the,their,them,then,there,these,they,this,tis,to,too,'
+    'twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,'
+    'will,with,would,yet,you,your').lower().split(',')
+
+
+def is_stopped(str):
+    '''文字がストップワードか判定
+    大文字小文字は区別しない
+
+    return
+    ストップワード：True
+    それ以外：False
+    '''
+    return str.lower in stop_words
+
+
+# assert 条件式, 条件式がFalseの場合に出力するメッセージ
+# assert文を使うと例外が出るとエラーを返し、停止してくれる
+# 正しく抽出されるテスト
+assert is_stopped('a')  # リストの先頭
+assert is_stopped('your')  # リストの末尾
+assert is_stopped('neigher')  # リストの中間
+assert is_stopped('OWN')  # 大小文字の同一視
+assert is_stopped('as')
+assert is_stopped('eigher')
+assert is_stopped('him')
+assert is_stopped('likely')
+
+# 誤検出のテスト
