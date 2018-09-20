1a941c88cda536ff018eba1c9b953ad783115c9c
junya <hatiro7code@gmail.com>
2018-09-20 22:05:20 +0900

    20180920 writing

--- 85.py
+++ 85.py
@@ -1,7 +1,16 @@
 # 次元圧縮の手法・・・削ったりクラスターに分けたり。
 # その中の一つが主成分分析(PCA)
 # 元のデータを変換。その時、最もばらつきが大きくなるものを選ぶ。
-from scipy imort sparse, io
+from scipy import sparse, io
+import sklearn.decomposition
 
 file_in = "84.txt"
 file_out = "85.txt"
+
+# 行列読み込み
+matrix = io.loadmat(file_in)['matrix_x']
+
+# 次元圧縮
+clf = sklearn.decomposition.TruncatedSVD(300)
+matrix_to_300 = clf.fit_transform(matrix)
+io.savemat(file_out, {'matrix300': matrix_to_300})
