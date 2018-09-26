6cdca4a8a6fbe75a3d90060dbe6d5c1a6b67bbe5
junya <hatiro7code@gmail.com>
2018-09-26 23:01:26 +0900

    20180926 wrote but need check

--- 88.py
+++ 88.py
@@ -19,3 +19,25 @@ def cos_sim(vec_a, vec_b):
         return np.dot(vec_a, vec_b) / norm_ab
     else:
         return -1  # 分母が0だと最低値を吐かせる
+
+
+# 辞書読み込み
+with open(f_in, "rt") as data_file:
+    dict_index = pickle.load(data_file)
+
+# 行列読み込み
+matrix_to_300 = io.loadmat(f_in2)["matrix_to_300"]
+# io.loadmatで呼び出す際には、キーを用いて呼び出す。
+
+# "England"とのコサイン類似度を算出
+vec_England = matrix_to_300[dict_index["England"]]
+distances = [cos_sim(vec_England, matrix_to_300[i]) for i in
+            range(len(dict_index))]
+# np.argsort()は並び替えた時のindexを返す
+# sort()は並び替えた結果を返す。
+
+# 上位10件を表示
+index_sorted = np.argsort(distances)
+keys = list(dict_index.keys())
+for index in index_sorted[-2:-12:-1]:
+    print("{}\t{}".format(keys[index], distances[index]))
