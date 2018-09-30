8821b5655b45539900f3c708f7e917658f34aad7
junya <hatiro7code@gmail.com>
2018-09-30 22:31:16 +0900

    20180930 wrote need check

--- 90.py
+++ 90.py
@@ -5,14 +5,16 @@ from scipy import io
 import word2vec
 
 f_in = "81.txt"
-f_word2vec = "vectors.txt"
-f_dict_index = "84.txt"
-f_matrix = "85.txt"
+f_word2vec = "85_word2vec.txt"
+f_dict_index = "85_dict"
+f_matrix = "85_matrix"
 
 # word2vecでベクトル化。出力までやってくれる
+# 1行目に用語数と次元数が空白区切で出力
+# 2行目以降1行1用語、各行に用語と対応する各次元の値300個が空白区切で出力
 word2vec.word2vec(train=f_in, output=f_word2vec,
                   size=300, threads=4, binary=0)
-#
+# word2vecを用いた結果生成された行列をよみこむ。辞書も作成
 with open(f_word2vec, 'rt') as data_file:
     vec_info = data_file.readline().split()  # 先頭行のみ読み込み
     size_dict = int(vec_info[0])
@@ -23,3 +25,10 @@ with open(f_word2vec, 'rt') as data_file:
 
     for i, line in enumerate(data_file):
         vec_value = line.strip().split()
+        dict_index[vec_value[0]] = i  # 値をキー、インデックスがvalue
+        matrix[i] = vec_value[1:]
+
+# 結果の書き出し
+io.savemat(f_matrix, {"matrix_300": matrix})
+with open(f_dict_index, "wb") as data_file:
+    pickle.dump(dict_index, data_file)
