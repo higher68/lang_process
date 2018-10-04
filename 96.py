a6cda05d36af0e491f7c1410b572977bcd7a9ef1
junya <hatiro7code@gmail.com>
2018-10-05 07:50:49 +0900

    20181005 wrote need check

--- 96.py
+++ 96.py
@@ -20,6 +20,7 @@ matrix_300_old = io.loadmat(f_matrix)["matrix_300"]
 # 辞書にある用語のみの行列を作成
 dict_index_new = OrderedDict()
 matrix_300_new = np.empty([0, 300], dtype=np.float64)
+# 0行、300列の行列
 count = 0
 
 with open(f_countries_plus, "rt") as data_file:
@@ -27,8 +28,14 @@ with open(f_countries_plus, "rt") as data_file:
         try:
             word = line.strip().replace(" ", "_")
             index = dict_index_old[word]
-            matrix_new = np.vstack([matrix_300_old, matrix_300_new[index]])
+            # np.vstackはndarray同士を結合する。行を追加
+            # matrix_300_old[index]]で、単語ベクトルに入力した単語が変換される。
+            matrix_300_new = np.vstack([matrix_300_new, matrix_300_old[index]])
             dict_index_new[word] = count
             count += 1
         except:
             pass
+# 結果の書き出し
+io.savemat(f_matrix_new, {"matrix_300": matrix_300_new})
+with open(f_dict_new, "wb") as data_file:
+    pickle.dump(dict_index_new, data_file)
